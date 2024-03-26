d5949ae5acbd00ddd1cb2efc698b4479f2283675
asalisinasa <asalisinasa@gmail.com>
2024-03-27 01:17:34 +0700

    Chapter 3: Optimizing images

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'], display: 'swap',
+variable: '--font-inter', });
 export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
